94b72ddb328b86bde37632642048b07b9927a7c2
VIKASH-5938 <134520842+VIKASH-5938@users.noreply.github.com>
2025-02-28 13:15:54 +0530

    Update dark.py

--- dark.py
+++ dark.py
@@ -53,7 +53,7 @@ async def start(update: Update, context: CallbackContext):
     )
     await context.bot.send_message(chat_id=chat_id, text=message, parse_mode='Markdown')
 
-async def ninja(update: Update, context: CallbackContext):
+async def dark(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     args = context.args
 
